94a42df24b6f82dc121400446c2672b9e4d24ae9
ishika1510 <ishikasaha413@gmail.com>
2026-03-01 00:59:04 +0530

    Reverse an array

---
+++ reverseArray.java
@@ -0,0 +1,13 @@
+public static int[] reverse(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+        }
+        return arr;
+    }
